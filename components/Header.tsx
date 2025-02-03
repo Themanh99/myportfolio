3fd02c130afe395148849f2e8641e3c117dd0c7b
manhct <manhct@gmail.com>
2025-02-03 17:06:08 +0700

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from './ui/button'
+
+const Header = () => {
+  return (
+    <header className='py-8 xl:py12 text-white'>
+        <div className='container mx-auto'>
+            <Link href='/'>
+                <h1 className='text-4xl font-semibold'>The Manh<span className='text-accent'>.</span></h1>
+            </Link>
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
